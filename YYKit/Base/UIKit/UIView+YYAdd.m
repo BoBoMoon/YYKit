e4007b323383fb4c81bed6d6e9d6292e22f371cc
BoBo <>
2019-03-05 15:19:47 +0800

    Revert "修改UIview分类，避免冲突"
    This reverts commit 70797c187742dc331002d7651dd3cdc6e9db3137.

--- YYKit/Base/UIKit/UIView+YYAdd.m
+++ YYKit/Base/UIKit/UIView+YYAdd.m
@@ -173,97 +173,97 @@ YYSYNTH_DUMMY_CLASS(UIView_YYAdd)
     return rect;
 }
 
-- (CGFloat)yy_left {
+- (CGFloat)left {
     return self.frame.origin.x;
 }
 
-- (void)setyy_left:(CGFloat)x {
+- (void)setLeft:(CGFloat)x {
     CGRect frame = self.frame;
     frame.origin.x = x;
     self.frame = frame;
 }
 
-- (CGFloat)yy_top {
+- (CGFloat)top {
     return self.frame.origin.y;
 }
 
-- (void)setyy_top:(CGFloat)y {
+- (void)setTop:(CGFloat)y {
     CGRect frame = self.frame;
     frame.origin.y = y;
     self.frame = frame;
 }
 
-- (CGFloat)yy_right {
+- (CGFloat)right {
     return self.frame.origin.x + self.frame.size.width;
 }
 
-- (void)yy_right:(CGFloat)right {
+- (void)setRight:(CGFloat)right {
     CGRect frame = self.frame;
     frame.origin.x = right - frame.size.width;
     self.frame = frame;
 }
 
-- (CGFloat)yy_bottom {
+- (CGFloat)bottom {
     return self.frame.origin.y + self.frame.size.height;
 }
 
-- (void)setyy_bottom:(CGFloat)bottom {
+- (void)setBottom:(CGFloat)bottom {
     CGRect frame = self.frame;
     frame.origin.y = bottom - frame.size.height;
     self.frame = frame;
 }
 
-- (CGFloat)yy_width {
+- (CGFloat)width {
     return self.frame.size.width;
 }
 
-- (void)setyy_width:(CGFloat)width {
+- (void)setWidth:(CGFloat)width {
     CGRect frame = self.frame;
     frame.size.width = width;
     self.frame = frame;
 }
 
-- (CGFloat)yy_height {
+- (CGFloat)height {
     return self.frame.size.height;
 }
 
-- (void)setyy_height:(CGFloat)height {
+- (void)setHeight:(CGFloat)height {
     CGRect frame = self.frame;
     frame.size.height = height;
     self.frame = frame;
 }
 
-- (CGFloat)yy_centerX {
+- (CGFloat)centerX {
     return self.center.x;
 }
 
-- (void)setyy_centerX:(CGFloat)centerX {
+- (void)setCenterX:(CGFloat)centerX {
     self.center = CGPointMake(centerX, self.center.y);
 }
 
-- (CGFloat)yy_centerY {
+- (CGFloat)centerY {
     return self.center.y;
 }
 
-- (void)setyy_centerY:(CGFloat)centerY {
+- (void)setCenterY:(CGFloat)centerY {
     self.center = CGPointMake(self.center.x, centerY);
 }
 
-- (CGPoint)yy_origin {
+- (CGPoint)origin {
     return self.frame.origin;
 }
 
-- (void)setyy_origin:(CGPoint)origin {
+- (void)setOrigin:(CGPoint)origin {
     CGRect frame = self.frame;
     frame.origin = origin;
     self.frame = frame;
 }
 
-- (CGSize)yy_size {
+- (CGSize)size {
     return self.frame.size;
 }
 
-- (void)setyy_size:(CGSize)size {
+- (void)setSize:(CGSize)size {
     CGRect frame = self.frame;
     frame.size = size;
     self.frame = frame;
